6272b4da22c9adc365c722bf7d2f43409f518f51
Simon Shi <lordsimon.shi@gmail.com>
2020-02-26 02:03:26 +0800

    improve output format.

--- find_all_ed2k_or_magnet.user.js
+++ find_all_ed2k_or_magnet.user.js
@@ -20,17 +20,37 @@
     var results = new Array()
     var results_text = ""
     var split_count = 0
+    var last_text = ""
+    var last_type = ""
+    var type_ed2k = false
+    var type_magnet = false
+    var current_type = ""
     for(i in herf_list){
         var url_text = herf_list[i].href
         if(url_text == null){
             continue
         }
+        if(url_text == last_text){
+            continue
+        }
+        last_text = url_text
+        type_ed2k = url_text.startsWith("ed2k:")
+        type_magnet = url_text.startsWith("magnet")
+        if(type_ed2k){
+            current_type = "type_ed2k"
+        }else if(type_magnet){
+            current_type = "type_magnet"
+        }else{
+            current_type = last_type
+        }
+
         if(url_text.startsWith("ed2k:") || url_text.startsWith("magnet")){
             split_count = split_count + 1
             results.push(url_text)
             try{
-                if(split_count % 15 == 0){
-
+                if(split_count == 1){
+                    results_text = decodeURI(url_text)
+                }else if(current_type != last_type){
                     results_text = results_text + "\n\n\n\n" + decodeURI(url_text)
                 }else{
                     results_text = results_text + "\n" + decodeURI(url_text)
@@ -41,6 +61,7 @@
                 results_text = results_text + "\n" + url_text
             }
         }
+        last_type = current_type
     }
     if(split_count<=0){
         console.log("No ed2k or magnet found. return.")
